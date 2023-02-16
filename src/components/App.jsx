31fc51de0e7e874cc35762e32fc567e4ad71b345
Maryna <yasinskamarina@gmail.com>
2023-02-16 22:16:47 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { FriendForm } from './FriendForm/FriendForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+const LOCAL_KEY = 'contacts';
+
+export class App extends Component {
+  state = {
+    contacts: this.getLocalData() ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  getLocalData() {
+    const localData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    return localData;
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+    }
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(this.state.contacts));
+  }
+
+  addContacts = contact => {
+    const isExist = this.state.contacts.some(({ name }) => {
+      return contact.name === name;
+    });
+    if (isExist) {
+      alert(`${contact.name} is already in contacts!`);
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { id: nanoid(), ...contact }],
+      };
+    });
+  };
+  deleteContacts = e => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== e.target.id
+        ),
+      };
+    });
+  };
+
+  handleFilterChange = e => {
+    const { value } = e.target;
+    this.setState({ filter: value });
+  };
+
+  render() {
+    const filterContacts = this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .trim()
+        .includes(this.state.filter.toLowerCase())
+    );
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <FriendForm onSubmit={this.addContacts} />
+        <h2>Contacts</h2>
+        <Filter onChange={this.handleFilterChange} value={this.state.filter} />
+        <ContactsList
+          contacts={filterContacts}
+          onBtnDelete={this.deleteContacts}
+        />
+      </>
+    );
+  }
+}
---
+++ src/components/FriendForm/FriendForm.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendForm.module.css';
+
+export class FriendForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={css.formCnt}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit" className={css.addBtn}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+FriendForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
